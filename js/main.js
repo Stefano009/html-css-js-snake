0644b84a94d5aec8e516951da5b661053d20eb35
Stefano009 <stefano.funaro@gmail.com>
2021-08-06 17:42:58 +0200

    comments

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 //creo l'array di spazi che fungerà da snake
 let snake;
-let reset = document.getElementById('snake-ground')
-
-// for (let i = 1; i <= cell; i++){
-//     let cell = `<div id = "cell" onclick = "onClick" data-cell="${i}" class = "cell">${i}</div>`;
-//     let templateCell = document.createElement('DIV');
-//     templateCell.classList.add('square');
-//     templateCell.innerHTML = cell;
-//     document.getElementById('campo').appendChild(templateCell);
-// }
+let reset = document.getElementById('snake-ground');
+//event start and reset
 document.getElementById('start').addEventListener('click',
     function() {
         snakeCreation();
