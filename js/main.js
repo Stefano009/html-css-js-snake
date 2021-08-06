5cec9e9df91c517b7271aeb56045f2123836ff60
Stefano009 <stefano.funaro@gmail.com>
2021-08-06 12:14:25 +0200

    function_snake_creator_implemented

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 //creo l'array di spazi che fungerà da snake
-let snake = ['','','','','',''];
+let snake = [];
+let snakeLength = 6;
+
+// for (let i = 1; i <= cell; i++){
+//     let cell = `<div id = "cell" onclick = "onClick" data-cell="${i}" class = "cell">${i}</div>`;
+//     let templateCell = document.createElement('DIV');
+//     templateCell.classList.add('square');
+//     templateCell.innerHTML = cell;
+//     document.getElementById('campo').appendChild(templateCell);
+// }
+
+//functions
+function snakeCreation(lenght) {
+    for (let i = 0; i < lenght; i++) {
+        snake[i] = `<div class = "snake-square"></div>`;
+    }
+    let snakeBody = document.createElement('DIV');
+    snakeBody.innerHTML = snake;
+    document.getElementById('snake-ground').appendChild(snakeBody);    
+}
